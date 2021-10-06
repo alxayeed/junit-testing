e007cd92e5ca8a4ab4acccb1f86d969cc89a6f6f
alxayeed <alxayeed@gmail.com>
2021-10-06 16:08:19 +0600

    assetThrows

--- src/test/java/com/alxayeed/jtesting/ContactManagerTest.java
+++ src/test/java/com/alxayeed/jtesting/ContactManagerTest.java
@@ -1,5 +1,6 @@
 package com.alxayeed.jtesting;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,7 +10,8 @@ import java.util.function.BooleanSupplier;
 class ContactManagerTests {
 
 	@Test
-	public void shouldCreateContact() {
+	@DisplayName("Test if contact is successfully created or not")
+	public void testAddContact() {
 		ContactManager contactManager = new ContactManager();
 		contactManager.addContact("Al", "Sayeed", "01683338978");
 		Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
@@ -21,4 +23,32 @@ class ContactManagerTests {
 				.findAny()
 				.isPresent());
 	}
+
+	@Test
+	@DisplayName("Test if the firstName is not null")
+	public void testfirstNameNotNull(){
+		ContactManager contactManager = new ContactManager();
+		Assertions.assertThrows(RuntimeException.class, ()->{
+			contactManager.addContact(null, "Sayeed", "01683338978");
+		});
+	}
+
+	@Test
+	@DisplayName("Test if the lastName is not null")
+	public void testLastNameNotNull(){
+		ContactManager contactManager = new ContactManager();
+		Assertions.assertThrows(RuntimeException.class, ()->{
+			contactManager.addContact("Al", null, "01683338978");
+		});
+	}
+
+	@Test
+	@DisplayName("Test if the phoneNumber is not null")
+	public void testPhoneNumberNotNull(){
+		ContactManager contactManager = new ContactManager();
+		Assertions.assertThrows(RuntimeException.class, ()->{
+			contactManager.addContact("Al", "Sayeed",null);
+		});
+	}
+
 }
