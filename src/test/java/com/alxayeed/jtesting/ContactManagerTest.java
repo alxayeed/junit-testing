82dc11205b706e7f07502bcef04792367059e3d0
alxayeed <alxayeed@gmail.com>
2021-10-07 11:27:27 +0600

    redundant code removed

--- src/test/java/com/alxayeed/jtesting/ContactManagerTest.java
+++ src/test/java/com/alxayeed/jtesting/ContactManagerTest.java
@@ -20,7 +20,6 @@ class ContactManagerTests {
 	@Test
 	@DisplayName("Test if contact is successfully created or not")
 	public void testAddContact() {
-		ContactManager contactManager = new ContactManager();
 		contactManager.addContact("Al", "Sayeed", "01683338978");
 		Assertions.assertFalse(contactManager.getAllContacts().isEmpty());
 		Assertions.assertEquals(1, contactManager.getAllContacts().size());
@@ -35,7 +34,6 @@ class ContactManagerTests {
 	@Test
 	@DisplayName("Test if the firstName is not null")
 	public void testfirstNameNotNull(){
-		ContactManager contactManager = new ContactManager();
 		Assertions.assertThrows(RuntimeException.class, ()->{
 			contactManager.addContact(null, "Sayeed", "01683338978");
 		});
@@ -44,7 +42,6 @@ class ContactManagerTests {
 	@Test
 	@DisplayName("Test if the lastName is not null")
 	public void testLastNameNotNull(){
-		ContactManager contactManager = new ContactManager();
 		Assertions.assertThrows(RuntimeException.class, ()->{
 			contactManager.addContact("Al", null, "01683338978");
 		});
@@ -53,7 +50,6 @@ class ContactManagerTests {
 	@Test
 	@DisplayName("Test if the phoneNumber is not null")
 	public void testPhoneNumberNotNull(){
-		ContactManager contactManager = new ContactManager();
 		Assertions.assertThrows(RuntimeException.class, ()->{
 			contactManager.addContact("Al", "Sayeed",null);
 		});
@@ -63,7 +59,6 @@ class ContactManagerTests {
 	@DisplayName("Test Disable OS")
 	@DisabledOnOs(value = OS.LINUX, disabledReason = "Test Disabled on Linux OS")
 	public void testDisabledOs(){
-		ContactManager contactManager = new ContactManager();
 		Assertions.assertThrows(RuntimeException.class, ()->{
 			contactManager.addContact("Al", "Sayeed",null);
 		});
@@ -73,11 +68,14 @@ class ContactManagerTests {
 	@DisplayName("Test Enable OS")
 	@EnabledOnOs(value = OS.MAC, disabledReason = "Test Only on Mac OS")
 	public void testEnabledOs(){
-		ContactManager contactManager = new ContactManager();
 		Assertions.assertThrows(RuntimeException.class, ()->{
 			contactManager.addContact("Al", "Sayeed",null);
 		});
 	}
+
+
+
+
 
 	@AfterAll
 	public void tearDownAll(){
